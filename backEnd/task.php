3d8c1d3e1691a4d0a5a1328c5e10c35a53add04b
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-08 14:26:54 +0100

    add function for get data from my json

--- backEnd/task.php
+++ backEnd/task.php
@@ -1,6 +1,8 @@
 <?php
     //recupero i dati dal file json che ho creato
-    $contentData = file_get_contents("myData.json")
-    
-
+    $contentData = file_get_contents("myData.json");
+    //devo inserire nell'header della richiesta http che è di tipo json
+    header("Content-Type: application/json");
+    //stampo nella pagina php il content del file
+    echo $contentData;
 ?>
