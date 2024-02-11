bc418806af636e452ddbf02dd6b0278aa2255903
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-11 19:06:14 +0100

    bonus 5 add updateTaskTodo, can change task

--- backEnd/updateTaskTodo.php
+++ backEnd/updateTaskTodo.php
@@ -6,14 +6,20 @@
     //trasformo attraverso il decode i file obj in array 
     $contentData = json_decode($contentDataJson, true);
    
+    //converto la stringa done in tipo booleano assegnandola ad una variabilew
+    $doneBoolean = ($_POST['done'] === "true") ? true : false;
     //creo il nuovo task come array da inserire successivamente
-    $newTask = [
+   
+    $updateTask = [
         'name' => $_POST['name'],
-        'done' => false
+        'done' => $doneBoolean 
     ];
-    var_dump($newTask );
+    var_dump("questo è l'elemnto". $updateTask);
+    //recupero l'indice della voce da eliminare
+    $index = intval($_POST['indice']);
+    var_dump($index);
     //aggiungo ai data il nuovo task
-    $contentData[] = $newTask;
+    $contentData[$index] = $updateTask;
     //riconverto i miei data array in json
     $newContentDataJson = json_encode($contentData);
     //scrivo nel file json
--- frontEnd/js/scripts.js
+++ frontEnd/js/scripts.js
@@ -6,8 +6,9 @@ createApp({
     data() {
         return {
             message: 'Todo list php',
-            myData:[],
+            myData: [],
             newTask: "",
+            flagEdit: []
         };
     },
     methods:{
@@ -18,27 +19,34 @@ createApp({
                 this.myData = response.data;
                 console.log(this.myData);
             })
+            for (let i = 0; i < this.myData.length; i++) {
+                this.flagEdit.push(true)
+            }
+            console.log(this.flagEdit)
         },
         //chiamata api per caricare un nuovo task
         addTodoJson(){
+            console.log("chiamata add:", this.myData, typeof this.myData)
             axios.post("http://localhost/Boolean/PrimaParteBackEnd/php-todo-list-json/backEnd/putNewTodo.php",
-                {
-                    name : this.newTask
+            {
+                name : this.newTask
+            }
+            ,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
                 }
-                ,
-                {
-                    headers: {
-                      'Content-Type': 'multipart/form-data'
-                    }
-                }
+            }
             )
             .then((response) => {
                 console.log(response)
                 this.addTask();
+                
             })
         },
+        //cancellazione del task in array e nel file jsone tramite api
         deleteTask(j){
-            this.myData.splice(j,1);
+            
             console.log(j)
             axios.post("http://localhost/boolean/PrimaParteBackEnd/php-todo-list-json/backEnd/cancelTaskTodo.php",
                 {
@@ -56,6 +64,26 @@ createApp({
                 this.myData.splice(j,1);
             })
         },
+        editTask(j){
+            this.flagEdit[j]= !(this.flagEdit[j])
+            console.log("chiamata add:", this.myData, typeof this.myData)
+            axios.post("http://localhost/Boolean/PrimaParteBackEnd/php-todo-list-json/backEnd/updateTaskTodo.php",
+            {
+                name : this.myData[j].name,
+                done : this.myData[j].done,
+                indice: j
+            }
+            ,
+            {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+            )
+            .then((response) => {
+                console.log(response)
+            })
+        },
         reverseDone(j){
             console.log("hai clicclato sull'elemento n:",j)
             this.myData[j].done = !(this.myData[j].done);
@@ -69,25 +97,25 @@ createApp({
                     'Content-Type': 'multipart/form-data'
                 }
             }
-        )
-        .then((response) => {
-            console.log(response);
-        })
+            )
+            .then((response) => {
+                console.log(response);
+            })
         }
         ,
         addTask(){
             if(this.newTask.trim().length > 3){
+                console.log("questo è mio Data", this.myData, typeof this.myData)
                 this.myData.push({name:this.newTask, done: false,})
             }
             this.newTask = "";
             
-        },
-        deleteTodo(j){
-            this.myData.splice(j,1);
         }
     },
     mounted(){
         this.searchData();
+    },
+    updated(){
     }
   // Monto l'istanza di Vue in pagina
 }).mount('#app');
